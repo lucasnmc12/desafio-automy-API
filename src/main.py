4b8cc2478b3ef5daf7ba54badda84f4544f8bb84
lucasnmc12 <lucas.nogueira20mc@gmail.com>
2025-05-10 13:51:54 -0300

    mensagens sobre as baterias

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from auth import obter_token
 from query import buscar_baterias
+from mensagens import gerar_mensagem
+
 
 
 if __name__ == "__main__":
@@ -19,5 +21,10 @@ if token:
         print(f"Registro {i}: ")
         for chave, valor in item.items():
             print(f"{chave}: {valor}")
+
+    mensagem = gerar_mensagem(dados)
+
+    print(mensagem)
+
 else:
     print("Nao foi possivel autenticar")
---
+++ src/mensagens.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+def gerar_mensagem(baterias: list[dict]) -> str:
+    hoje = datetime.now().date()
+    agendadas = []
+    passadas =[]
+
+
+    for bateria in baterias:
+        data_str = bateria.get("data_agendamento")
+        try:
+            data = datetime.strptime(data_str, "%d/%m/%Y").date()
+            if data >= hoje:
+                agendadas.append(bateria)
+            else:
+                passadas.append(bateria)
+        except ValueError:
+            print(f"Data inválida:  {data_str}")
+
+    mensagem = ("")
+
+    if not agendadas:
+        print("Nenhuma amostra de próximas baterias")
+    else:
+        deseja_proximas = input("Deseja visualizar suas próximas baterias?\n1 - Sim\n2 - Não\n>").strip()
+        if deseja_proximas == "1":
+            mensagem += "Proximas baterias agendadas:\n"
+            for b in agendadas:
+                mensagem += f"- {b['data_agendamento']} às {b['horario_agendamento']} - {b['qtde_pessoas']} pessoas\n"
+            print(mensagem)
+        else:
+            print("Proximas baterias não exibidas")
+        
+    if not passadas:
+        print("Nenhuma amostra de baterias passadas")
+    else:
+        if passadas and isinstance(passadas[0], dict):
+            deseja_passadas = input("Deseja visualizar suas baterias passadas?\n1 - Sim\n2 - Não\n>").strip()
+            if deseja_passadas == "1":
+                mensagem += "\n Baterias passadas: \n"
+                for b in passadas:
+                    mensagem += f"- {b['data_agendamento']} às {b['horario_agendamento']} — {b['qtde_pessoas']} pessoas\n"
+                print(mensagem)
+            else:
+                print("Baterias passadas não exibidas")
+            
+
+    return mensagem
+            
